c13488ecd7e979f84fe07e9fce5e1e976dd7600e
kiefer256 <kiefermiskiw@gmail.com>
2020-03-17 18:43:46 -0500

    Refactor: cleanup Lift and make use off less specialty classes

--- YearSpecificFeatures/2020/Lift.java
+++ YearSpecificFeatures/2020/Lift.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems;
 
-// imports
-
 // ctre 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
@@ -9,41 +7,27 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 // wpilib 
 import edu.wpi.first.wpilibj.Relay;
 
+//to prevent the lift from falling, this subsystem uses an electric solenoid controlled by a relay to lock itself
+
 public class Lift {
 
     private static TalonSRX m_lift;
     private static Relay m_lock;
 
     public static double m_power;
-    public static boolean m_valid;
     public static double m_delay;
-    public static double m_state;
+    public static double m_unlocked;
 
-    public static void init() {
-
-        m_valid = true;
-
-        m_lift = new TalonSRX(RobotMap.kLiftMotor);
+    public static void init(int motorPort, int relayPort) {
+        m_lift = new TalonSRX(motorPort);
         m_lift.setInverted(false);
-        try {
-            m_lock = new Relay(RobotMap.kLiftSolenoid);
-
-        }
-        catch(Exception e)
-        {
-            m_valid = false;
-            System.out.println("LIFT INVALID " + e);
-        }
+		
+        m_lock = new Relay(relayPort);
         clear();
     }
 
 
     public static void clear() {
-
-        if(!m_valid)
-
-            return;
-
         m_power = 0;
         
         m_lift.set(ControlMode.PercentOutput, 0);
@@ -53,16 +37,13 @@ public class Lift {
     }
 
     public static void update() {
-
-        if(!m_valid)
-            return;
         if(m_power > 0) {
-            if(m_state == 0 ) {
-                m_state = 1;
+            if(m_unlocked == 0) {
+                m_unlocked = 1;
                 m_delay = RT.getTime() + 1;
                 m_lock.set(Relay.Value.kReverse);
             }
-            else if(RT.getTime() > m_delay && m_state == 1) {
+            else if(RT.getTime() > m_delay && m_unlocked == 1) {
 
                 m_lift.set(ControlMode.PercentOutput, m_power);
 
@@ -71,12 +52,12 @@ public class Lift {
         }
         else if(m_power < 0) {
             m_lift.set(ControlMode.PercentOutput, m_power);
-            m_state = 0;
+            m_unlocked = 0;
         }
         else {
             m_lift.set(ControlMode.PercentOutput, 0);
             m_lock.set(Relay.Value.kOff);
-            m_state = 0;
+            m_unlocked = 0;
         }
     }
 
