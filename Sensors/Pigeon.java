85836f3c0ffd1b2e1ba5c4bd4b02fbb56071e5bc
Forrest Johnson <forrest.z.johnson@gmail.com>
2020-03-17 14:11:07 -0500

    Mod: Pigeon snippet refined

--- Sensors/Pigeon.java
+++ Sensors/Pigeon.java
@@ -1,33 +1,44 @@
 package frc.robot.subsystems.sensors;
 
+// ctre 
 import com.ctre.phoenix.sensors.PigeonIMU;
-import frc.robot.mapping.RobotMap;
 
 public class Pigeon {
 
 	public static double m_heading;
 	public static PigeonIMU m_pigeon;
+	public static int port = 1; 
 
 	public static void init() {
-		m_pigeon = new PigeonIMU(RobotMap.kPidgeonPort);
+
+		m_pigeon = new PigeonIMU(port);
+
 		reset();
+
 	}
 
-	// Updates m_heading variable
 	public static void update() {
+
 		m_heading = m_pigeon.getFusedHeading() % 360;
-		if (m_heading < 0)
-			m_heading += 360;
+
+		if (m_heading < 0) {
+
+			m_heading += 360; 
+
+		}
+
 	}
 
-	// Returns robot direction
 	public static double getHeading() {
+
 		return m_heading;
+
 	}
 
-	// Resets Pigeon's heading
 	public static void reset() {
+
 		m_pigeon.setFusedHeading(0);
+
 	}
 
 }
