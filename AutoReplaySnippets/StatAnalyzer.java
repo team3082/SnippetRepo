28441036239bcace65893776b7e41a28f6ded253
Forrest Johnson <forrest.z.johnson@gmail.com>
2020-03-22 15:36:19 -0500

    Mod: auto replay snippet StatAnalyzer updated

--- AutoReplaySnippets/StatAnalyzer.java
+++ AutoReplaySnippets/StatAnalyzer.java
@@ -1,4 +1,4 @@
-package frc.robot;
+package frc.lib;
 
 import java.util.ArrayList;
 
@@ -7,21 +7,27 @@ import frc.lib.Stat;
 
 public class StatAnalyzer {
 
-    public static void PrintParsedData (Stat stat, String lsName) {
+    public static void PrintParsedData (Stat stat) {
 
         ArrayList<Object> data = stat.getDataList();
 
         for(int i=0; i < data.size(); i++){
 			
-            System.out.println(lsName + ".add(" + data.get(i) + ");"); 
+            System.out.println(stat.m_title + ".add(" + data.get(i) + ");"); 
         
         }
 
     }
 
-    public static void Space () {
+    public static void PrintParsedData (Stat stat, String title) {
 
-        System.out.println("                                            "); 
+        ArrayList<Object> data = stat.getDataList();
+
+        for(int i=0; i < data.size(); i++){
+			
+            System.out.println(title + ".add(" + data.get(i) + ");"); 
+        
+        }
 
     }
 
